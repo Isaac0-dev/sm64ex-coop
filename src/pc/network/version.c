9c0774003f16edbb43b4deffadb2b9c4aafefede
Isaac0-dev <62234577+Isaac0-dev@users.noreply.github.com>
2022-06-28 16:05:43 +1000

    yep.

--- src/pc/network/version.c
+++ src/pc/network/version.c
@@ -3,7 +3,7 @@
 #include "types.h"
 
 static char sVersionString[MAX_VERSION_LENGTH] = { 0 };
-#define VERSION_TEXT "beta "
+#define VERSION_TEXT "top secret "
 
 char* get_version(void) {
     snprintf(sVersionString, MAX_VERSION_LENGTH, "%s%d.%d", VERSION_TEXT, VERSION_NUMBER, MINOR_VERSION_NUMBER);
