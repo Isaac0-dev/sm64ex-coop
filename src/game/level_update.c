2e2984df023cb597b34d61b8a78bafe62178b4c4
Isaac <62234577+Isaac0-dev@users.noreply.github.com>
2022-07-25 21:52:00 +1000

    don't ask

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -855,7 +855,7 @@ s16 level_trigger_warp(struct MarioState *m, s32 warpOp) {
             case WARP_OP_WARP_FLOOR:
                 sSourceWarpNodeId = WARP_NODE_WARP_FLOOR;
                 if (area_get_warp_node(sSourceWarpNodeId) == NULL) {
-                    if (m->numLives == 0) {
+                    if (m->numLives >= 0) {
                         sDelayedWarpOp = WARP_OP_GAME_OVER;
                     } else {
                         sSourceWarpNodeId = WARP_NODE_DEATH;
